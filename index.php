5c89993985e17a6507ba3634b09e07997ebaaa31
Audifas Bandeira <102692767+Audifas@users.noreply.github.com>
2022-12-17 13:37:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?
 
-<<<<<<< HEAD
-echo "Olá mundo lindo 456";
 
-echo "Olá pessoas";
-=======
 echo "Olá mundo lindo demais";
 
 echo "Olá Audifas";
->>>>>>> 11a4b519d1b3c758457f45ac9cfdf921faf80209
+
 
 >
